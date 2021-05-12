9e0eb48cd1de66bddddb0cde245453643d27590c
washington de souza <souzawashingtonssj@hotmail.com>
2021-05-11 22:26:26 -0300

    novas funcionalidades

--- index.php
+++ index.php
@@ -1,33 +1,50 @@
 <!doctype html>
 <html lang="pt-br">
-  <head>
+
+<head>
     <title>Aprendendo Git e Github</title>
     <!-- Required meta tags -->
     <meta charset="utf-8">
     <meta name="viewport" content="width=device-width, initial-scale=1, shrink-to-fit=no">
     <style>
-    *{list-style-type: none;text-decoration:none}
-    
-    </style>
-    <!-- Bootstrap CSS -->
-    <link rel="stylesheet" href="https://stackpath.bootstrapcdn.com/bootstrap/4.3.1/css/bootstrap.min.css" integrity="sha384-ggOyR0iXCbMQv3Xipma34MD+dH/1fQ784/j6cY/iJTQUOhcWr7x9JvoRxT2MZw1T" crossorigin="anonymous">
-  </head>
-  <body>
-  <header>
-  <nav>
-  <ul>
-  <li><a href="#">Página Inicial</a></li>
-  <li><a href="#">Cadatros de Produtos</a></li>
-  </ul>
-  </nav>
-  </header>
-      <main>
+    * {
+        list-style-type: none;
+        text-decoration: none
+    }
+    html{width: 100%;}
+    nav{}
+    nav ul{width: 100%; height: 40px; background-color: gray;}
+    nav ul li{float: left; padding: 8px 8px 8px 0;}
+    nav ul li a{text-decoration: none; color: white; }
+    nav ul li a:hover{text-decoration: none; color: black; background-color: blanchedalmond;}
 
-      </main>
+    </style>
+    <!--vscode://vscode.github-authentication/did-authenticate?windowid=1&code=10a16e73bcb2f3c1ffd2&state=57166f57-4242-425e-895a-b0e1300e282e-->
+    <!-- Bootstrap CSS -->
+    <link rel="stylesheet" href="https://stackpath.bootstrapcdn.com/bootstrap/4.3.1/css/bootstrap.min.css">
+</head>
+
+<body>
+    <header>
+        <nav class="nav">
+            <ul>
+                <li><a href="#">Página Inicial</a></li>
+                <li><a href="#">Cadatros de Produtos</a></li>
+            </ul>
+        </nav>
+    </header>
+    <main>
+
+    </main>
     <!-- Optional JavaScript -->
     <!-- jQuery first, then Popper.js, then Bootstrap JS -->
-    <script src="https://code.jquery.com/jquery-3.3.1.slim.min.js" integrity="sha384-q8i/X+965DzO0rT7abK41JStQIAqVgRVzpbzo5smXKp4YfRvH+8abtTE1Pi6jizo" crossorigin="anonymous"></script>
-    <script src="https://cdnjs.cloudflare.com/ajax/libs/popper.js/1.14.7/umd/popper.min.js" integrity="sha384-UO2eT0CpHqdSJQ6hJty5KVphtPhzWj9WO1clHTMGa3JDZwrnQq4sF86dIHNDz0W1" crossorigin="anonymous"></script>
-    <script src="https://stackpath.bootstrapcdn.com/bootstrap/4.3.1/js/bootstrap.min.js" integrity="sha384-JjSmVgyd0p3pXB1rRibZUAYoIIy6OrQ6VrjIEaFf/nJGzIxFDsf4x0xIM+B07jRM" crossorigin="anonymous"></script>
-  </body>
+    <script src="https://code.jquery.com/jquery-3.3.1.slim.min.js"
+        integrity="sha384-q8i/X+965DzO0rT7abK41JStQIAqVgRVzpbzo5smXKp4YfRvH+8abtTE1Pi6jizo" crossorigin="anonymous">
+    </script>
+    <script src="https://cdnjs.cloudflare.com/ajax/libs/popper.js/1.14.7/umd/popper.min.js">
+    </script>
+    <script src="https://stackpath.bootstrapcdn.com/bootstrap/4.3.1/js/bootstrap.min.js">
+    </script>
+</body>
+
 </html>
