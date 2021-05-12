d188fa30a8b2fb635dff70ffe9875126acb47067
washington de souza <souzawashingtonssj@hotmail.com>
2021-05-11 21:38:27 -0300

    novas alterações

--- index.php
+++ index.php
@@ -1,19 +1,28 @@
 <!doctype html>
-<html lang="en">
+<html lang="pt-br">
   <head>
-    <title>Aprendendo Git e Github
-    </title>
+    <title>Aprendendo Git e Github</title>
     <!-- Required meta tags -->
     <meta charset="utf-8">
     <meta name="viewport" content="width=device-width, initial-scale=1, shrink-to-fit=no">
-
+    <style>
+    *{list-style-type: none;text-decoration:none}
+    </style>
     <!-- Bootstrap CSS -->
     <link rel="stylesheet" href="https://stackpath.bootstrapcdn.com/bootstrap/4.3.1/css/bootstrap.min.css" integrity="sha384-ggOyR0iXCbMQv3Xipma34MD+dH/1fQ784/j6cY/iJTQUOhcWr7x9JvoRxT2MZw1T" crossorigin="anonymous">
   </head>
   <body>
-      <h1>Olá mundo maravilhoso!!</h1>
+  <header>
+  <nav>
+  <ul>
+  <li><a href="#">Página Inicial</a></li>
+  <li><a href="#">Cadatros de Produtos</a></li>
+  </ul>
+  </nav>
+  </header>
+      <main>
 
-      <h3>Ola meu amor</h3>
+      </main>
     <!-- Optional JavaScript -->
     <!-- jQuery first, then Popper.js, then Bootstrap JS -->
     <script src="https://code.jquery.com/jquery-3.3.1.slim.min.js" integrity="sha384-q8i/X+965DzO0rT7abK41JStQIAqVgRVzpbzo5smXKp4YfRvH+8abtTE1Pi6jizo" crossorigin="anonymous"></script>
